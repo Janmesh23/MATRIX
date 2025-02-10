05260b1444bae425c01cd96b06f5189166fdff1f
Sanjana-chennu <sanjana.chennupati@gmail.com>
2025-02-11 02:36:51 +0530

    made changes in background.js and manifest.json

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-const API_KEY = "AIzaSyCZx34avqXbxcZMWFz9xSeY6f93ktYEzgg";
-const API_URL= `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${API_KEY}`;
-
 import CONFIG from './config.js';
 
+// Use API Key from config.js
+const API_URL = `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${GOOGLE_SAFE_BROWSING_API_KEY}`;
 async function checkURL(url) {
   const requestBody = {
       client: {
